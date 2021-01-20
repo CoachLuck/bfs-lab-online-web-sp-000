61cb75f8a67374772a8b05c284484918cce4eeab
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 04:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 function bfs(rootNode, vert, edges) {
-  // Set the root distance to 0
   rootNode.distance = 0
 
-  // initialize our queue and order of search
   let queue = [rootNode]
   let order = [rootNode]
 
-  // Begin the search
   while (queue.length > 0) {
       let node = queue.shift()
       let adjacent = findAdjacent(node.name, vert, edges)
-
-      // add found to our final list in the order we get them
       order = order.concat(adjacent)
-
-      // mark where we have been
       markDistanceAndPredecessor(node, adjacent)
-
-      // add to queue
       queue = queue.concat(adjacent)
   }
 
